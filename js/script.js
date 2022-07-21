5ea66585d086ef9995f84cf6e5fa20454967cd34
Victor Greavu <viktorens10mc@gmail.com>
2022-07-21 16:44:46 +0300

    fix for menu button

--- js/script.js
+++ js/script.js
@@ -140,7 +140,7 @@ var prevScrollpos = window.pageYOffset;
 
 async function handleNavbar() {
     var currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
+    if (prevScrollpos > currentScrollPos || window.pageYOffset <= 50) {
         document.getElementById("navbar").style.top = "0";
         document.getElementById("navbar-mobile").style.top = "0";
     } else {
